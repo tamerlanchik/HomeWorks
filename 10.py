9ad6307336446c194541847f16b43228f42bfc79
tamerlanchik <andreykochnov@yandex.ru>
2016-02-23 14:12:04 +0300

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,15 @@
+from random import randint
+a=[randint(1, 200) for i in range(10)]
+print(*a)
+n=int(input('Введите N: '))
+New=[]
+for i in a:
+    ans=0
+    temp=i
+    for k in range(3): #вычисление суммы цифр
+        ans+=temp%10
+        temp//=10   
+    if ans==n:
+        New.append(i)   #добавляет число в конец массива, как по заданию
+if len(New)!=0: print(*New)
+else: print('Ничего не найдено :( ')
