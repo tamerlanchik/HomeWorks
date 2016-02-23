a70d1726ea5160a3f5e68165e9d92d24536777e4
tamerlanchik <andreykochnov@yandex.ru>
2016-02-23 13:03:44 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,5 +1,6 @@
 from random import randint
-a=[randint(0, 10) for i in range(10)]
-temp=
-for i in range(len(a)):
-    
+a=[randint(0, 10) for i in range(11)] #создается массив из 11 рандомных чисел
+print(*a)
+for i in range(1, len(a), 2): #проходит цикл от 1 до len(a) не включая с шагом 2
+    a[i], a[i-1]=a[i-1], a[i] #элементы меняются местами
+print(*a)
