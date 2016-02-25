d2858548266888101fa5cf10ecdce5cb2b42e5a7
tamerlanchik <andreykochnov@yandex.ru>
2016-02-25 17:23:58 +0300

    Delete 6.py

--- 6.py
+++
@@ -1,6 +0,0 @@
-from random import randint
-a=[randint(0, 10) for i in range(11)] #создаетс€ массив из 11 рандомных чисел
-print(*a)
-for i in range(1, len(a), 2): #проходит цикл от 1 до len(a) не включа€ с шагом 2
-    a[i], a[i-1]=a[i-1], a[i] #элементы мен€ютс€ местами
-print(*a)
