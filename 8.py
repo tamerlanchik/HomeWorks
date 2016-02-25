5be25c0b10b03279774394d5ba08a9af2938e37a
tamerlanchik <andreykochnov@yandex.ru>
2016-02-25 17:24:12 +0300

    Delete 8.py

--- 8.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-a=[randint(1, 10) for i in range(10)]
-print(*a)
-print('Введите K и M. Помните, нумерация элементов ведется с нуля.')
-K, M=int(input('K: ')), int(input('M: '))
-for i in range((M-K+1)//2):
-    a[K+i], a[M-i]=a[M-i], a[K+i]
-print(*a)
