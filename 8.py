dbd3391d3f453a936066ef882e46fdfb684ff7f7
Andrey K <andreykochnov@yandex.ru>
2016-02-25 17:33:46 +0300

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,7 +1,7 @@
 from random import randint
 a=[randint(1, 10) for i in range(10)]
 print(*a)
-print('Ââåäèòå K è M. Ïîìíèòå, íóìåðàöèÿ ýëåìåíòîâ âåäåòñÿ ñ íóëÿ.')
+print('Введите K и M. Помните, нумерация элементов ведется с нуля.')
 K, M=int(input('K: ')), int(input('M: '))
 for i in range((M-K+1)//2):
     a[K+i], a[M-i]=a[M-i], a[K+i]
